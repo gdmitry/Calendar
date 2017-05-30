ced3eef02b606255004c846cbfbd08819c30a4ea
dmitryg <dmitryg@hexagontech.co.il>
2017-05-30 12:59:06 +0300

    table

--- components/EventsTable.js
+++ components/EventsTable.js
@@ -3,10 +3,33 @@ import _ from "lodash";
 
 export class EventsTable extends React.Component {
     render() {
-       
+
         return (
-            <table>
-              
+            <table className="ui celled table">
+                <thead className="">
+                    <tr className="">
+                        <th className="">Header</th>
+                        <th className="">Header</th>
+                        <th className="">Header</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                    <tr className="">
+                        <td className="">Cell</td>
+                        <td className="">Cell</td>
+                        <td className="">Cell</td>
+                    </tr>
+                    <tr className="">
+                        <td className="">Cell</td>
+                        <td className="">Cell</td>
+                        <td className="">Cell</td>
+                    </tr>
+                    <tr className="">
+                        <td className="">Cell</td>
+                        <td className="">Cell</td>
+                        <td className="">Cell</td>
+                    </tr>
+                </tbody>                
             </table>
         );
     }
